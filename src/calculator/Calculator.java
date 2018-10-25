e13a4718bfaf9179273d6f498b8de8fa56dd1e8f
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-10-24 22:13:04 -0500

    refactor UI so it has a main method; add CalcFunctions class

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,17 +5,28 @@
  */
 package calculator;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author Mike Schulenberg
  */
-public class Calculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        UI ui = new UI();
+public class Calculator extends JFrame {
+    private final int WIDTH = 250;
+    private final int HEIGHT = 350;
+    
+    public Calculator() {
+        prepUI();
     }
     
+    public static void main(String[] args) {
+        new Calculator();
+    }
+    
+    private void prepUI() {
+        setTitle("Calculator");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }  
 }
