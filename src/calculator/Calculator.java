46d6c75f4a573e441c2674baf6d5c5f6981227ea
Mike Schulenberg <mike.schulenberg@gmail.com>
2018-11-03 22:19:24 -0500

    add JLabel to display expressions

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -14,8 +14,9 @@ import java.awt.*;
  */
 public class Calculator extends JFrame {
     private final int WIDTH = 250;
-    private final int HEIGHT = 350;
+    private final int HEIGHT = 280;
     private JPanel mainPanel;
+    private JLabel display;
     
     public Calculator() {
         prepUI();
@@ -41,129 +42,154 @@ public class Calculator extends JFrame {
     private void prepComponents() {
         mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
         
-        prepRow1(gbc);
-        prepRow2(gbc);
-        prepRow3(gbc);
-        prepRow4(gbc);
-        prepRow5(gbc);
-        prepRow6(gbc);
+        prepRow1();
+        prepRow2();
+        prepRow3();
+        prepRow4();
+        prepRow5();
+        prepRow6();
         
         add(mainPanel);
     }
     
-    private void prepRow1(GridBagConstraints gbc) {
+    private void prepRow1() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 0, 25, 0);
+        gbc.gridwidth = 4;
+        gbc.ipady = 10;
+        display = new JLabel("test");
+        display.setOpaque(true);
+        display.setBackground(Color.WHITE);
+        display.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        mainPanel.add(display, gbc);
     }
     
-    private void prepRow2(GridBagConstraints gbc) {      
+    private void prepRow2() {      
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         JButton bClear = new JButton("C");
         mainPanel.add(bClear, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         JButton bLeftParen = new JButton("(");
         mainPanel.add(bLeftParen, gbc);
         
         gbc.gridx = 2;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         JButton bRightParen = new JButton(")");
         mainPanel.add(bRightParen, gbc);
         
         gbc.gridx = 3;
-        gbc.gridy = 2;      
+        gbc.gridy = 1;      
         JButton bDivide = new JButton("\u00F7"); // ÷ sign
         mainPanel.add(bDivide, gbc); 
     }
     
-    private void prepRow3(GridBagConstraints gbc) {      
+    private void prepRow3() {      
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 2;
         JButton b7 = new JButton("7");
         mainPanel.add(b7, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 3;
+        gbc.gridy = 2;
         JButton b8 = new JButton("8");
         mainPanel.add(b8, gbc);
         
         gbc.gridx = 2;
-        gbc.gridy = 3;
+        gbc.gridy = 2;
         JButton b9 = new JButton("9");
         mainPanel.add(b9, gbc);
         
         gbc.gridx = 3;
-        gbc.gridy = 3;
+        gbc.gridy = 2;
         JButton bMultiply = new JButton("\u00D7"); // × sign
         mainPanel.add(bMultiply, gbc);
     }
     
-    private void prepRow4(GridBagConstraints gbc) {
+    private void prepRow4() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
         gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridy = 3;
         JButton b4 = new JButton("4");
         mainPanel.add(b4, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 4;
+        gbc.gridy = 3;
         JButton b5 = new JButton("5");
         mainPanel.add(b5, gbc);
         
         gbc.gridx = 2;
-        gbc.gridy = 4;
+        gbc.gridy = 3;
         JButton b6 = new JButton("6");
         mainPanel.add(b6, gbc);
         
         gbc.gridx = 3;
-        gbc.gridy = 4;
+        gbc.gridy = 3;
         JButton bAdd = new JButton("+");
         mainPanel.add(bAdd, gbc);
     }
     
-    private void prepRow5(GridBagConstraints gbc) {
+    private void prepRow5() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
         gbc.gridx = 0;
-        gbc.gridy = 5;
+        gbc.gridy = 4;
         JButton b1 = new JButton("1");
         mainPanel.add(b1, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 5;
+        gbc.gridy = 4;
         JButton b2 = new JButton("2");
         mainPanel.add(b2, gbc);
         
         gbc.gridx = 2;
-        gbc.gridy = 5;
+        gbc.gridy = 4;
         JButton b3 = new JButton("3");
         mainPanel.add(b3, gbc);
         
         gbc.gridx = 3;
-        gbc.gridy = 5;
+        gbc.gridy = 4;
         JButton bSubtract = new JButton("-");
         mainPanel.add(bSubtract, gbc);
     }
     
-    private void prepRow6(GridBagConstraints gbc) {
+    private void prepRow6() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        
         gbc.gridx = 0;
-        gbc.gridy = 6;
+        gbc.gridy = 5;
         JButton b0 = new JButton("0");
         mainPanel.add(b0, gbc);
         
         gbc.gridx = 1;
-        gbc.gridy = 6;
+        gbc.gridy = 5;
         JButton bDecimal = new JButton(".");
         mainPanel.add(bDecimal, gbc);
         
         gbc.gridx = 2;
-        gbc.gridy = 6;
+        gbc.gridy = 5;
         JButton bBack = new JButton("\u00AB"); // « symbol
         mainPanel.add(bBack, gbc);
         
         gbc.gridx = 3;
-        gbc.gridy = 6;
+        gbc.gridy = 5;
         JButton bEquals = new JButton("=");
         mainPanel.add(bEquals, gbc);
     }
